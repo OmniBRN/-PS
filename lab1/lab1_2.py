1a7ac9aada15cf6efa6301a0d9c8df45ad89dfbe
Tudor Boureanu <tudor04boureanu@gmail.com>
2025-10-04 21:51:18 +0300

    Am facut exercitiul 2 cred

--- lab1/lab1_2.py
+++ lab1/lab1_2.py
@@ -10,34 +10,44 @@ Ts1 = 1/400
 t1 = np.linspace(0, 1, 1600)
 x = np.sin(2*np.pi*1/Ts1*t1)
 fig, axs = plt.subplots()
+fig.suptitle("Semnal Sinusoidal de Frecventa 400Hz")
 axs.plot(t1,x)
+fig.savefig("2_400Hz.pdf")
 plt.show()
 
 # 2.b)
 Ts2 = 1/800
-t2 = np.linspace(0, 3)
+t2 = np.linspace(0, 3, 3*4800)
 y = np.sin(2*np.pi*1/Ts2*t2)
 fig, axs = plt.subplots()
+fig.suptitle("Semnal Sinusoidal de Frecventa 800Hz care sa dureze 3 secunde")
 axs.plot(t2,y)
+fig.savefig("2_800Hz3s.pdf")
 plt.show()
 
 # 2.c)
-t3 = np.linspace(0, 3, 720)
+t3 = np.linspace(0, 1, 960)
 z = np.mod(t3, 1)
 fig, axs = plt.subplots()
+fig.suptitle("Semnal Sawtooth de frecventa 240Hz")
 axs.plot(t3,z)
+fig.savefig("2_Sawtooth240Hz.pdf")
 plt.show()
 
 # 2.d)
-t4 = np.linspace(0, 3, 300)
+t4 = np.linspace(0, 1, 1200)
 t = np.sign(np.sin(2*np.pi*t4*300))
 fig, axs = plt.subplots()
+fig.suptitle("Semnal Square de frecventa 300Hz")
 axs.plot(t4,t)
+fig.savefig("2_Square300Hz.pdf")
 plt.show()
 
 # 2.e)
 t5 = np.random.rand(128,128)
 plt.imshow(t5)
+plt.title("Semnal 2D aleator")
+plt.savefig("2_Semnal2Daleator.pdf")
 plt.show()
 
 # 2. f)
@@ -51,15 +61,6 @@ def shgradient():
 
 t6 = shgradient()
 plt.imshow(t6)
+plt.title("Semnal 2D - Gradient")
+plt.savefig("2_Semnal2Dgradient.pdf")
 plt.show()
-
-
-# fig, axs = plt.subplots(4)
-# fig.suptitle('test')
-# axs.plot(t1,x)
-# axs.plot(t2,y)
-# axs.plot(t3,z)
-# axs.plot(t4,t)
-
-# plt.show()
-
