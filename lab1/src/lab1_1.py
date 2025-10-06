37ea845b000175c430849761f90e24d324836804
Tudor Boureanu <tudor04boureanu@gmail.com>
2025-10-06 20:18:29 +0300

    Am adaugat etichete si rezolvat o greseala la primul exercitiu

--- lab1/src/lab1_1.py
+++ lab1/src/lab1_1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 matplotlib.use('qtAgg')
 
 # 1. a)
-x = np.arange(0, 0.03+0.0005, 0.0005)
+x = np.linspace(0, 0.03, int(0.03/0.0005))
 
 # 1. b)
 
@@ -17,11 +17,14 @@ fig.suptitle('Continuu')
 axs[0].plot(x,y_x)
 axs[1].plot(x,y_y)
 axs[2].plot(x,y_z)
+plt.xlabel("Timp")
+plt.ylabel("Semnal")
+fig.savefig("1_Continuu.pdf")
 plt.show()
 
 # 1. c)
 Ts = 1/200
-t_e = np.arange(0, 0.03+Ts, Ts)
+t_e = np.linspace(0, 0.03, 6)
 x_e = np.cos(520*np.pi*t_e + np.pi/3)
 y_e = np.cos(280*np.pi*t_e - np.pi/3)
 z_e = np.cos(120*np.pi*t_e + np.pi/3)
@@ -31,4 +34,7 @@ fig2.suptitle('Esantionate')
 axs2[0].stem(t_e, x_e)
 axs2[1].stem(t_e, y_e)
 axs2[2].stem(t_e, z_e)
+plt.xlabel("Timp")
+plt.ylabel("Semnal")
+fig2.savefig("1_Esantionate.pdf")
 plt.show()
